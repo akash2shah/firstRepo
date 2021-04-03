5b4cd63c921b238a5e23d367e50d12d257722278
aks <aks@aks>
2021-04-03 23:29:57 +0530

    setup

---
+++ Demo/src/com/FinacleIntegrationRestClient.java
@@ -0,0 +1,13 @@
+package com;
+
+public class FinacleIntegrationRestClient {
+
+	public void connectFinacle()
+	{
+		
+	}
+	public void generateRequestXML()
+	{
+		//call method based on requested API
+	}
+}
